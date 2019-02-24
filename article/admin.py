438ba8cc0bc779c5da3f21f89a69d7aa56286b9e
ziho <hzhhzh1999@gmail.com>
2019-02-24 23:14:38 +0800

    Article Model showing details

--- article/admin.py
+++ article/admin.py
@@ -3,5 +3,8 @@ from .models import Ariticle
 
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("title", "content")
 
-admin.site.register(Ariticle)
+
+admin.site.register(Ariticle, ArticleAdmin)
